cde808112ed5755e0f05a695eaad03ae0655043c
Vitor Assis <vitorassis997@gmail.com>
2024-06-06 14:06:48 -0300

    update vidFlow

--- script.js
+++ script.js
@@ -24,3 +24,26 @@ async function buscarEMostrarVideos() {
 }
 
 buscarEMostrarVideos()
+
+const barraDePesquisa = document.querySelector(".pesquisar__input")
+
+barraDePesquisa.addEventListener("input", filtrarPesquisa)
+
+ function filtrarPesquisa() {
+    const videos = document.querySelectorAll(".videos__item")
+
+    if(barraDePesquisa.value != "") {
+        for(let video of videos){
+            let titulo = video.querySelector(".titulo-video").textContent.toLowerCase();
+            let valorFiltro = barraDePesquisa.value.toLowerCase();
+
+            if(!titulo.includes(valorFiltro)) {
+                video.style.display = "none"
+            } else {
+                video.style.display = "block"
+            }
+        }
+    } else {
+        videos.forEach(video => video.style.display = 'block');
+    }
+ }
