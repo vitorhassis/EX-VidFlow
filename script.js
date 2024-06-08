8b391d6e3f2d6842a3765320a6b4308dc197a8eb
Vitor Assis <vitorassis997@gmail.com>
2024-06-08 19:14:26 -0300

    uptade vidFlow
    Finalizando o exercício, consumindo uma api em JS.

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ async function buscarEMostrarVideos() {
                     <img class="img-canal" src="${video.imagem} alt = "logo do canal">
                     <h3 class="titulo-video">${video.titulo}</h3>
                     <p class ="titulo-canal">${video.descricao}</p>
+                    <p class="categoria" hidden>${video.categoria}</p>
                 </div>
                 </li>
                 `; 
@@ -48,18 +49,27 @@ barraDePesquisa.addEventListener("input", filtrarPesquisa);
     }
  }
 
- const botaoCategoria = document.querySelectorAll("superior__item");
-
- function filtrarPorCategoria(filtro) {
-    const videos = document.querySelectorAll(".videos__item")
-    for(let video of videos) {
-        
-    }
- }
+ const botaoCategoria = document.querySelectorAll(".superior__item");
 
  botaoCategoria.forEach((botao) => {
     let nomeCategoria = botao.getAttribute("name");
-    botao.addEventListener("click", () => filtrarPorCategoria(nomeCategoria))
+    botao.addEventListener("click", () => filtrarPorCategoria(nomeCategoria));
  })
+
+ function filtrarPorCategoria(filtro) {
+    const videos = document.querySelectorAll(".videos__item");
+    for(let video of videos) {
+        let categoria = video.querySelector(".categoria").textContent.toLowerCase();
+        let valorFiltro = filtro.toLowerCase();
+
+        if(!categoria.includes(valorFiltro) && valorFiltro != 'tudo') {
+            video.style.display = "none";
+        } else {
+            video.style.display = "block";
+        }
+    }
+ }
+
+ 
 
  
